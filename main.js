8d6c421a0f422f8db017212634a61ba94e52f2a7
Lebedev Gregory <lga91@mail.ru>
2025-02-25 14:31:27 +0300

    add lesson 2 learn git

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+{
 const designation = 'Apple';
 const amount = 5;
 const category = 'fruits';
@@ -8,7 +9,21 @@ const price = 100;
 let sum = amount * price
 
 console.log(designation);
-console.log(sum)
+console.log(`общая сумма товара: ${amount * price}`);
+
+}
 
 
 
+{
+    const designation = 'Orange';
+    const amount = 2;
+    const category = 'fruits';
+    const price = 150;
+
+    let sum = amount * price
+
+    console.log(designation);
+    console.log(`общая сумма товара: ${amount * price}`);
+
+}
